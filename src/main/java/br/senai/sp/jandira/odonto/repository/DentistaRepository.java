02e49a28b53f4ec6ff20fb68f210a2a31339a3af
Lazaro Carvalho <lazaro.oliveiras@hotmail.com>
2020-02-03 17:00:22 -0300

    Adicionando recursos

--- src/main/java/br/senai/sp/jandira/odonto/repository/DentistaRepository.java
+++ src/main/java/br/senai/sp/jandira/odonto/repository/DentistaRepository.java
@@ -1,9 +1,10 @@
 package br.senai.sp.jandira.odonto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import br.senai.sp.jandira.odonto.model.Dentista;
 
 public interface DentistaRepository extends JpaRepository<Dentista, Long>{
 
+	
+	
 }
